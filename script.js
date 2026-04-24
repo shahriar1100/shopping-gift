b5298483dc8896d525bbb454ec042a66bff06d17
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-04-24 12:31:18 +0600

    add some javascript

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const happyUi = document.getElementById("happyUi")
 const sadUi = document.getElementById("sadUi")
+const mainBox = document.getElementById("mainBox")
 
 
-const number = document.getElementById("number")
+const shoppingAmount = document.getElementById("shoppingAmount")
 const age = document.getElementById("age")
 
+const submitBtn = document.getElementById("submitBtn")
+
+submitBtn.addEventListener("click", function (){
+    const amountValue = shoppingAmount.value
+    const ageValue = age.value
+    if(amountValue > 5000 && ageValue > 20){
+        happyUi.style.display = "block";
+        mainBox.style.display = "none";
+    }else{
+        sadUi.style.display = "block";
+        mainBox.style.display = "none";
+    }
+})
