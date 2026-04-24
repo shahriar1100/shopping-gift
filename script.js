8f474e31b3dd8b568f15d50652ecad8b304adc41
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-04-24 11:46:34 +0600

    get id in javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+const happyUi = document.getElementById("happyUi")
+const sadUi = document.getElementById("sadUi")
+
+
+const number = document.getElementById("number")
+const age = document.getElementById("age")
+
